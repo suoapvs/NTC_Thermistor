bacf6f043eff1df042a53d87aaa64397df87a486
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2018-02-01 18:27:47 +0200

    Added the NTC_Thermistor class.

---
+++ src/NTC_Thermistor.cpp
@@ -0,0 +1,90 @@
+/**
+	The class implements a set of methods of the NTCThermistor.h 
+	interface for working with a NTC thermistor and reading 
+	a temperature in Celsius, Fahrenheit and Kelvin.
+
+	Created by Yurii Salimov, February, 2018.
+	Released into the public domain.
+*/
+#include "NTC_Thermistor.h"
+
+NTC_Thermistor::NTC_Thermistor(
+	const int pin,
+	const double referenceResistance,
+	const double nominalResistance,
+	const double nominalTemperatureCelsius,
+	const double bValue
+) {
+	this->pin = pin;
+	this->referenceResistance = referenceResistance;
+	this->nominalResistance = nominalResistance;
+	this->nominalTemperature = CELSIUS_TO_KELVINS(nominalTemperatureCelsius);
+	this->bValue = bValue;
+	pinMode(this->pin, INPUT_PULLUP);
+}
+
+/**
+	Reads and returns a temperature in Celsius.
+	Reads the temperature in Kelvin, 
+	converts in Celsius and return it.
+	@return temperature in Celsius.
+*/
+double NTC_Thermistor::readCelsius() {
+	const double kelvin = readKelvin();
+	return KELVINS_TO_CELSIUS(kelvin);
+}
+
+/**
+	Returns a temperature in Fahrenheit.
+	Reads a temperature in Kelvin, 
+	converts in Fahrenheit and return it.
+	@return temperature in Fahrenheit.
+*/
+double NTC_Thermistor::readFahrenheit() {
+	const double celsius = readKelvin();
+	return KELVINS_TO_FAHRENHEIT(celsius);
+}
+
+double NTC_Thermistor::readFarenheit() {
+	return readFahrenheit();
+}
+
+/**
+	Returns a temperature in Kelvin.
+	Reads the thermistor resistance, 
+	converts in Kelvin and return it:
+	1/K = 1/K0 + 1/B * ln(R/R0)
+	@return temperature in Kelvin.
+*/
+double NTC_Thermistor::readKelvin() {
+	const double resistance = readResistance();
+	const double inverseKelvin = 1.0 / this->nominalTemperature +
+		1.0 / this->bValue * log(resistance / this->nominalResistance);
+	return (1.0 / inverseKelvin);
+}
+
+/**
+	Calculates a resistance of the thermistor:
+	Converts a value of the thermistor sensor into a resistance.
+	R = R0 / (ADC / V - 1);
+	@return resistance of the thermistor sensor.
+*/
+double NTC_Thermistor::readResistance() {
+	const double voltage = readVoltage();
+	return this->referenceResistance / (ADC / voltage - 1);
+}
+
+/**
+	Reads a voltage from the thermistor analog port.
+	Takes READINGS_NUMBER samples in a row, 
+	with a slight delay.
+	@return average thermistor voltage.
+*/
+double NTC_Thermistor::readVoltage() {
+	double average = 0;
+	for (int i = 0; i < READINGS_NUMBER; i++) {
+		average += analogRead(this->pin);
+		delay(TIME_DELAY);
+	}
+	return (average / READINGS_NUMBER);
+}
---
+++ src/NTC_Thermistor.h
@@ -0,0 +1,163 @@
+/**
+	NTCThermoSensor.h - The interface describes a set of methods 
+	for working with a NTC thermistor and reading 
+	a temperature in Celsius, Fahrenheit and Kelvin.
+	
+	Instantiation, for example, to NTC 3950 thermistor:
+		NTCThermistor thermistor(A1, 8000, 100000, 25, 3950);
+
+	Read temperature:
+		thermistor.readCelsius();
+		thermistor.readKelvin();
+		thermistor.readFahrenheit();
+
+	Created by Yurii Salimov, February, 2018.
+	Released into the public domain.
+*/
+#ifndef NTC_THERMISTOR_H
+#define NTC_THERMISTOR_H
+
+#if defined(ARDUINO) && (ARDUINO >= 100)
+  #include <Arduino.h>
+#else
+  #include <WProgram.h>
+#endif 
+
+/**
+	Min value of Arduino ADC.
+*/
+#define ADC_MIN	0
+
+/**
+	Max value of Arduino ADC.
+*/
+#define ADC_MAX	1023
+
+/**
+	Values diapason of Arduino ADC
+*/
+#define ADC	(ADC_MAX - ADC_MIN)
+
+/**
+	How many readings are taken to determine a mean voltage. 
+	The more values, the longer a calibration is performed, 
+	but the readings will be more accurate.
+*/
+#define READINGS_NUMBER	5
+
+/**
+	Delay time between a temperature readings 
+	from the temperature sensor (ms).
+*/
+#define TIME_DELAY	10
+
+/**
+	Kelvin to Celsius conversion:
+		C = K - 273.15
+*/
+#define KELVINS_TO_CELSIUS(K) ((K - 273.15))
+
+/**
+	Celsius to Kelvin conversion:
+		K = C + 273.15
+*/
+#define CELSIUS_TO_KELVINS(C) ((C + 273.15))
+
+/**
+	Celsius to Fahrenheit conversion:
+		F = C * 9 / 5 + 32
+*/
+#define CELSIUS_TO_FAHRENHEIT(C) ((C * 9.0 / 5.0 + 32))
+
+/**
+	Kelvin to Fahrenheit conversion:
+	F = C * 9 / 5 + 32 = (K - 273.15) * 9 / 5 + 32
+*/
+#define KELVINS_TO_FAHRENHEIT(K) (CELSIUS_TO_FAHRENHEIT(KELVINS_TO_CELSIUS(K)))
+
+class NTC_Thermistor final {
+
+	private:
+		/**
+			Port number to be attached to the sensor.
+		*/
+		int pin = 0;
+
+		/**
+			Reference resistance of the thermistor.
+		*/
+		double referenceResistance = 0;
+
+		/**
+			Nominal resistance of the thermistor.
+		*/
+		double nominalResistance = 0;
+
+		/**
+			Nominal temperature of the thermistor.
+		*/
+		double nominalTemperature = 0;
+
+		/**
+			B-value of a thermistor.
+		*/
+		double bValue = 0;
+
+	public:
+		/**
+			Constructor.
+			@param pin - an analog port number to be attached 
+				to the thermistor.
+			@param referenceResistance - reference resistance.
+			@param nominalResistance - nominal resistance.
+			@param nominalTemperature - nominal temperature in Celsius.
+			@param bValue - b-value of a thermistor.
+		*/
+		NTC_Thermistor(
+			const int pin,
+			const double referenceResistance,
+			const double nominalResistance,
+			const double nominalTemperatureCelsius,
+			const double bValue
+		);
+
+		/**
+			Reads a temperature in Celsius.
+			@return temperature in Celsius.
+		*/
+		double readCelsius();
+
+		/**
+			Reads a temperature in Kelvin.
+			@return temperature in Kelvin.
+		*/
+		double readKelvin();
+		
+		/**
+			Reads a temperature in Fahrenheit.
+			@return temperature in Fahrenheit.
+		*/
+		double readFahrenheit();
+
+		/**
+			Returns a temperature in Fahrenheit.
+			(For older devices.)
+			@return temperature in Fahrenheit.
+		*/
+		 double readFarenheit();
+
+	private:
+		/**
+			Calculates a resistance of the thermistor:
+			@return resistance of the thermistor sensor.
+		*/
+		double readResistance();
+
+		/**
+			Reads a voltage from the thermistor.
+			@return thermistor voltage.
+		*/
+		double readVoltage();
+};
+
+#endif
