9039baf9b3153cc35b7f6a18aaf88eb675effb21
Mr. Alex <yuriy.alex.salimov@gmail.com>
2019-12-25 20:09:43 +0200

    Update Thermistor.h
    Fixed bugs

--- src/Thermistor.h
+++ src/Thermistor.h
@@ -30,7 +30,7 @@ class Thermistor {
       Destructor
       Deletes Thermistor instance.
     */
-    virtual ~SmoothThermistor() {};
+    virtual ~Thermistor() {};
 
 		/**
 			Reads a temperature in Celsius from the thermistor.
